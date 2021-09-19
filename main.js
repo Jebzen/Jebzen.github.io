fa80aad379c64c05c12f39d0a70fcdffb4ea2e3a
jebzen <jeb.bentzen@gmail.com>
2021-09-19 17:51:24 +0200

    Assignment

---
+++ main.js
@@ -0,0 +1,16 @@
+/* 
+    1. 7 event listeners
+
+    2. Needs a:
+        * String
+        * Number
+        * Array
+        * Object
+        * Callback functions
+        * Functions
+        * Input Fields
+        * Buttons
+        * Images
+
+    3. 250 lines of code
+*/
