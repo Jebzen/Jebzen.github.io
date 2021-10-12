381dd609fe3a375e621ed14a47080c23aeca0a30
Jeppe Bentzen <jeb.bentzen@gmail.com>
2021-10-12 12:26:47 +0200

    checkpoint

---
+++ Fun/main.js
@@ -0,0 +1,88 @@
+/*
+arr = [0,0,0,0,0,0,0,0,0,0];
+num = 0;
+
+for(i = 0; i < 1250; i++){
+
+    if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 1 && arr[6] == 1 && arr[7] == 1 && arr[8] == 1 && arr[9] == 1){
+        arr[9] = 0;
+        arr[8] = 0;
+        arr[7] = 0;
+        arr[6] = 0;
+        arr[5] = 0;
+        arr[4] = 0;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    }else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 1 && arr[6] == 1 && arr[7] == 1 && arr[8] == 1 && arr[9] == 0){
+        arr[9] = 1;
+        arr[8] = 0;
+        arr[7] = 0;
+        arr[6] = 0;
+        arr[5] = 0;
+        arr[4] = 0;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 1 && arr[6] == 1 && arr[7] == 1 && arr[8] == 0){
+        arr[8] = 1;
+        arr[7] = 0;
+        arr[6] = 0;
+        arr[5] = 0;
+        arr[4] = 0;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 1 && arr[6] == 1 && arr[7] == 0){
+        arr[7] = 1;
+        arr[6] = 0;
+        arr[5] = 0;
+        arr[4] = 0;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 1 && arr[6] == 0){
+        arr[6] = 1;
+        arr[5] = 0;
+        arr[4] = 0;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 0){
+        arr[5] = 1;
+        arr[4] = 0;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 0){
+        arr[4] = 1;
+        arr[3] = 0;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 0){
+        arr[3] = 1;
+        arr[2] = 0;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 1 && arr[2] == 0){
+        arr[2] = 1;
+        arr[1] = 0;
+        arr[0] = 0;
+    } else if(arr[0] == 1 && arr[1] == 0){
+        arr[1] = 1;
+        arr[0] = 0;
+    } else if(arr[0] == 0){
+        arr[0] = 1;
+    }
+
+    console.log(i);
+    console.log(arr);
+}
+*/
