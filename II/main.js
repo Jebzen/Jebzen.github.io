5be67bfb812749bc5670345803b46e97c1ddf679
jebzen <jeb.bentzen@gmail.com>
2022-01-09 16:34:00 +0100

    IV done

--- II/main.js
+++ II/main.js
@@ -113,15 +113,15 @@ console.log(notStrict());
 assignment("Show examples of how to use Filter, Map and Sort methods and how to chain the methods?");
 
 //Filter
-const users = [
-    {name: 'shaun', premium: true},
-    {name: 'yoshi', premium: false},
-    {name: 'mario', premium: false},
-    {name: 'chun-li', premium: true}
-];
+    const users = [
+        {name: 'shaun', premium: true},
+        {name: 'yoshi', premium: false},
+        {name: 'mario', premium: false},
+        {name: 'chun-li', premium: true}
+    ];
 
-const premiumUsers = users.filter(user => user.premium);
-console.log(premiumUsers);
+    const premiumUsers = users.filter(user => user.premium);
+    console.log(premiumUsers);
 
 //Map
     const prices = [20, 10, 30, 25, 15, 40, 80, 5];
