d8174dad51595bb776cc126f5b59f8fbd776d7de
jebzen <jeb.bentzen@gmail.com>
2022-01-09 13:44:19 +0100

    I (1/2)

--- old/js/weather.js
+++ old/js/weather.js
@@ -5,60 +5,60 @@ const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-  // destructure properties
-  const { cityDets, weather } = data;
+    // destructure properties
+    const { cityDets, weather } = data;
 
-  // update details template
-  details.innerHTML = `
-    <h5 class="my-3">${cityDets.EnglishName}</h5>
-    <div class="my-3">${weather.WeatherText}</div>
-    <div class="display-4 my-4">
-      <span>${weather.Temperature.Metric.Value}</span>
-      <span>&deg;C</span>
-    </div>
-  `;
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+    `;
 
-  // update the night/day & icon images
-  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-  icon.setAttribute('src', iconSrc);
-  
-  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-  time.setAttribute('src', timeSrc);
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+    const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
 
-  // remove the d-none class if present
-  if(card.classList.contains('d-none')){
-    card.classList.remove('d-none');
-  }
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
 };
 
 const updateCity = async (city) => {
 
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
-  return { cityDets, weather };
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    return { cityDets, weather };
 
 };
 
 if(localStorage && localStorage.getItem('Location')){
-  cityForm.city.value = localStorage.getItem('Location');
+    cityForm.city.value = localStorage.getItem('Location');
 
-  const city = cityForm.city.value.trim();
-  updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
+    const city = cityForm.city.value.trim();
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 }
 
 cityForm.addEventListener('submit', e => {
-  // prevent default action
-  e.preventDefault();
-  
-  // get city value
-  const city = cityForm.city.value.trim();
-  //cityForm.reset();
-  localStorage.setItem('Location', city);
+    // prevent default action
+    e.preventDefault();
+    
+    // get city value
+    const city = cityForm.city.value.trim();
+    //cityForm.reset();
+    localStorage.setItem('Location', city);
 
-  // update the ui with new city
-  updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
+    // update the ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 });
---
+++ ui-lib-project/src/index.js
@@ -0,0 +1,29 @@
+import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
+
+// create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)');
+});
