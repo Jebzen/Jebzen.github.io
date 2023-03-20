6984f2b879ff5af206900532aa665321ad1ed208
Jebzen <jeb.bentzen@gmail.com>
2023-03-20 12:33:44 +0100

    Project Starts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router";
 
 export default function Home() {
+	useEffect(() => {
+		document.title = "Test Home Page";
+	});
 	return (
 		<>
 			<h1>Home</h1>
