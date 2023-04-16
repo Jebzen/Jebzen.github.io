8ebd2cd489162a6f01a346df00cc933910c209e3
Jebzen <jeb.bentzen@gmail.com>
2023-04-16 19:46:29 +0200

    Dropdown animation :ballot_box_with_check: Proper text :ballot_box_with_check: Interactivity :ballot_box_with_check:

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/extended.scss";
 import "./styles/main.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
 	<React.StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,13 +9,30 @@ export default function Home() {
 	});
 	*/
 
+	function showEvent(e) {
+		if (e.key != undefined && e.key != "Enter") {
+			return;
+		}
+
+		const target = e.currentTarget.nextElementSibling;
+		const arrow = e.currentTarget.querySelector(".bi.bi-caret-right");
+
+		if (!target.classList.contains("hideable-open")) {
+			target.classList.add("hideable-open");
+			arrow.classList.add("ninetydeg");
+		} else {
+			target.classList.remove("hideable-open");
+			arrow.classList.remove("ninetydeg");
+		}
+	}
+
 	return (
 		<>
 			<main className="container">
 				<section className="hero mt-5">
 					<div className="bg-image mb-5 mb-md-0"></div>
 					<h1>Hello!</h1>
-					<h1>I'm Jeppe E. Bentzen</h1>
+					<h1>I'm Jeppe Emil Bentzen</h1>
 
 					<h1 className="profession">
 						<span>Web Developer</span>
@@ -65,12 +82,13 @@ export default function Home() {
 					<aside className="col-lg-4 d-none d-lg-flex sticky-top">
 						<section className="position-absolute mt-5" id="contact">
 							<article className="bg-primary p-3">
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Incidunt sunt aliquam amet consequuntur officiis odio saepe
-									obcaecati ipsam vitae, possimus architecto cupiditate sint
-									recusandae expedita earum autem eum excepturi laborum.
-								</p>
+								<h3 className="mb-3">Want to contact me?</h3>
+								<a
+									href="mailto:jeb.bentzen.work@gmail.com"
+									className="text-white"
+								>
+									jeb.bentzen.work@gmail.com
+								</a>
 							</article>
 						</section>
 					</aside>
@@ -81,55 +99,152 @@ export default function Home() {
 						</header>
 
 						<article id="bachelor" className="mb-4">
-							<header>
+							<header
+								className="clicker"
+								onClick={showEvent}
+								tabIndex={0}
+								onKeyDown={showEvent}
+							>
 								<h3>
-									Web Developer <i className="bi bi-caret-right-fill"></i>
+									Web Developer
+									<i
+										className="ps-1 bi bi-caret-right ninetydeg"
+										id="eduweb-arrow"
+									></i>
 								</h3>
 							</header>
-							<section className="hidable" id="eduweb">
+
+							<section className="hideable hideable-open" id="eduweb">
 								<header className="mb-4">
 									<h4>Bachelor</h4>
-									<h5>CPHBusiness Lyngby</h5>
+									<h5>
+										CPHBusiness Lyngby <span>2021 - 2023</span>
+									</h5>
 								</header>
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Incidunt sunt aliquam amet consequuntur officiis odio saepe
-									obcaecati ipsam vitae, possimus architecto cupiditate sint
-									recusandae expedita earum autem eum excepturi laborum.
-								</p>
+
+								<ul>
+									<li className="mb-3">
+										Front End Development <br />
+										<span className="badge text-bg-primary me-2">
+											Javascript
+										</span>
+										<span className="badge text-bg-primary me-2">
+											Typescript
+										</span>
+										<span className="badge text-bg-primary me-2">React</span>
+									</li>
+
+									<li className="mb-3">
+										Back End Development <br />
+										<span className="badge text-bg-primary me-2">C#</span>
+										<span className="badge text-bg-primary me-2">ASP.NET</span>
+										<span className="badge text-bg-primary me-2">Python</span>
+										<span className="badge text-bg-primary me-2">Testing</span>
+										<span className="badge text-bg-primary me-2">MSSQL</span>
+									</li>
+
+									<li className="mb-3">
+										User Experience & Design <br />
+										<span className="badge text-bg-primary me-2">
+											Desgon conventions
+										</span>
+										<span className="badge text-bg-primary me-2">
+											User testing
+										</span>
+										<span className="badge text-bg-primary me-2">
+											User flow
+										</span>
+										<span className="badge text-bg-primary me-2">
+											User recognition
+										</span>
+										<span className="badge text-bg-primary me-2">
+											Atomic design
+										</span>
+									</li>
+								</ul>
 							</section>
 						</article>
 
 						<article id="academic" className="mb-4">
-							<header>
-								<h3>Multimedia Designer</h3>
+							<header
+								className="clicker"
+								onClick={showEvent}
+								tabIndex={0}
+								onKeyDown={showEvent}
+							>
+								<h3>
+									Multimedia Designer
+									<i className="ps-1 bi bi-caret-right" id="edumul-arrow"></i>
+								</h3>
 							</header>
-							<section className="hidable" id="edumul">
+
+							<section className="hideable" id="edumul">
 								<header className="mb-4">
 									<h4>Academic Graduation</h4>
-									<h5>CPHBusiness Lyngby</h5>
+									<h5>
+										CPHBusiness Lyngby <span>2019 - 2021</span>
+									</h5>
 								</header>
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Incidunt sunt aliquam amet consequuntur officiis odio saepe
-									obcaecati ipsam vitae, possimus architecto cupiditate sint
-									recusandae expedita earum autem eum excepturi laborum.
-								</p>
+
+								<ul>
+									<li className="mb-3">
+										Front End Development <br />
+										<span className="badge text-bg-primary me-2">
+											Javascript
+										</span>
+										<span className="badge text-bg-primary me-2">HTML</span>
+										<span className="badge text-bg-primary me-2">CSS</span>
+										<span className="badge text-bg-primary me-2">SCSS</span>
+									</li>
+
+									<li className="mb-3">
+										Visual identity and Graphic Design <br />
+										<span className="badge text-bg-primary me-2">
+											Illustrator
+										</span>
+										<span className="badge text-bg-primary me-2">
+											Photoshop
+										</span>
+										<span className="badge text-bg-primary me-2">
+											Design concepts
+										</span>
+									</li>
+
+									<li className="mb-3">
+										Back End Development <br />
+										<span className="badge text-bg-primary me-2">PHP</span>
+										<span className="badge text-bg-primary me-2">MYSQL</span>
+										<span className="badge text-bg-primary me-2">
+											Server-Side Validation
+										</span>
+										<span className="badge text-bg-primary me-2">API's</span>
+									</li>
+								</ul>
 							</section>
 						</article>
 
 						<article id="htx" className="mb-4">
-							<header>
-								<h3>High School</h3>
-								<h4>HTX</h4>
-								<h5>Sukkertoppen HTX</h5>
+							<header
+								className="clicker"
+								onClick={showEvent}
+								tabIndex={0}
+								onKeyDown={showEvent}
+							>
+								<h3>
+									High School
+									<i
+										className="ps-1 bi bi-caret-right"
+										id="high-school-arrow"
+									></i>
+								</h3>
 							</header>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Incidunt sunt aliquam amet consequuntur officiis odio saepe
-								obcaecati ipsam vitae, possimus architecto cupiditate sint
-								recusandae expedita earum autem eum excepturi laborum.
-							</p>
+
+							<section className="hideable" id="high-school">
+								<header>
+									<h4>HTX</h4>
+									<h5>Sukkertoppen HTX 2017 - 2019</h5>
+								</header>
+							</section>
 						</article>
 					</section>
 
@@ -137,39 +252,84 @@ export default function Home() {
 						<header className="mb-4 fw-bold text-uppercase">
 							<h2>Experience</h2>
 						</header>
-						<article id="alphasol" className="mb-4">
-							<h3>Alpha Solutions | Internship</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Incidunt sunt aliquam amet consequuntur officiis odio saepe
-								obcaecati ipsam vitae, possimus architecto cupiditate sint
-								recusandae expedita earum autem eum excepturi laborum.
-							</p>
-						</article>
-						<article id="multimediaintern" className="mb-4">
-							<h3>Multimedia Designer | Internship</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Incidunt sunt aliquam amet consequuntur officiis odio saepe
-								obcaecati ipsam vitae, possimus architecto cupiditate sint
-								recusandae expedita earum autem eum excepturi laborum.
-							</p>
-						</article>
-					</section>
 
-					<section id="projects" className="col-lg-8 mb-5">
-						<header className="mb-4 fw-bold text-uppercase">
-							<h2>Projects</h2>
-						</header>
-						<article>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Incidunt sunt aliquam amet consequuntur officiis odio saepe
-								obcaecati ipsam vitae, possimus architecto cupiditate sint
-								recusandae expedita earum autem eum excepturi laborum.
-							</p>
+						<article id="alphasol" className="mb-4">
+							<header
+								className="clicker"
+								onClick={showEvent}
+								tabIndex={0}
+								onKeyDown={showEvent}
+							>
+								<h3>
+									Alpha Solutions
+									<i
+										className="ps-1 bi bi-caret-right"
+										id="high-school-arrow"
+									></i>
+								</h3>
+							</header>
+
+							<section className="hideable" id="exp-alhpa">
+								<h4>Internship</h4>
+								<ul>
+									<li>E-commerce webshop with a Headless CMS</li>
+									<li>Rendered Client sided</li>
+									<li>Integration with the search engine Solr.</li>
+									<li>Login with Google Authentication</li>
+									<li>CI/CD & system testing</li>
+									<li>Background task manager with Hangfire</li>
+								</ul>
+							</section>
+						</article>
+
+						<article id="multimediaintern" className="mb-4">
+							<header
+								className="clicker"
+								onClick={showEvent}
+								tabIndex={0}
+								onKeyDown={showEvent}
+							>
+								<h3>
+									Rich Media Specialist
+									<i
+										className="ps-1 bi bi-caret-right"
+										id="high-school-arrow"
+									></i>
+								</h3>
+							</header>
+
+							<section className="hideable" id="exp-multi">
+								<ul>
+									<li>Maintenance and development for 3 websites</li>
+									<li>WordPress development with Elementor</li>
+									<li>
+										Third party intergration with Google Analytics, WPbackup,
+										etc.
+									</li>
+									<li>
+										Custom made Webshop form with Javascript, PHP and MYSQL
+									</li>
+									<li>
+										Custom WordPress intergration, development and styling
+									</li>
+								</ul>
+							</section>
 						</article>
 					</section>
+				</section>
+
+				<section id="projects" className="mb-5">
+					<header className="mb-4 fw-bold text-uppercase">
+						<h2>Projects</h2>
+					</header>
+					<article>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+							sunt aliquam amet consequuntur officiis odio saepe obcaecati ipsam
+							vitae, possimus architecto cupiditate sint recusandae expedita
+							earum autem eum excepturi laborum.
+						</p>
+					</article>
 				</section>
 			</main>
 		</>
